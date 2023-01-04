6316f6be0057a556dc6ef6b2bedcfd6ba0dc22b2
vangtt <giangthanhvan12@gmail.com>
2023-01-04 10:22:10 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Control from "./component/Control";
+import ListStudent from "./component/ListStudent";
+import Form from "./component/Form";
+import React, { Component } from "react";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: false,
+      students: [
+        {studentsID: 'SV001', name: 'Trần Hải Yến', age: 24, gender: false, birthDate: "23/02/1999", address: "Hà Nội"},
+        {studentsID: 'SV002', name: 'Trương Minh Thu', age: 21, gender: false, birthDate: "10/09/2002", address: "Yên Bái"},
+        {studentsID: 'SV003', name: 'Đỗ Văn Chuân', age: 27, gender: true, birthDate: "25/05/1996", address: "Nam Định"}
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="row">
+          <div className="col-lg-7 grid-margin stretch-card">
+            <div className="card">
+              {/* START CONTROL */}
+              <Control></Control>
+              {/* END CONTROL */}
+              {/* START LIST STUDENT */}
+              <ListStudent listStudent={this.state.students}></ListStudent>
+              {/* END LIST STUDENT */}
+            </div>
+          </div>
+          {/* START FORM SINH VIEN */}
+          <Form ></Form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
